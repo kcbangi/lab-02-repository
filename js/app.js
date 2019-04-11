a6aa3cc0b7c045acfa8eb2c29b69e19de52b319b
Adrienne <adrienneeaston@gmail.com>
2019-04-11 14:03:48 -0700

    more work on js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
+//creates constructor function
+
 function Horn(horn) {
   this.name = horn.name;
   this.image_url = horn.image_url;
   this.hobbies = horn.hobbies;
 }
 
+// creates array with all objects from the constructor function
+
 Horn.allHorns =[];
+
+// declares function that clones the empty div elements, populates it with properties from each object, and deletes the emplty div. 
 
 Horn.prototype.render = function() {
   $('main').append('<div class="clone"></div>');
@@ -23,24 +29,75 @@ Horn.prototype.render = function() {
   hornClone.attr('class', this.name);
 }
 
+// creates a promise that once the json file is read, data from each object will be populated into the new div template.
+
 Horn.readJson = () => {
   $.get('data/page-1.json', 'json')
     .then(data => {
       data.forEach(item => {
         Horn.allHorns.push(new Horn(item));
-      })
+      });
+
+      // .then(Horn.loadHorns)
+
+      Horn.allHorns.forEach (horn => {
+        $('main').append(horn.render());
+      });
     })
-    .then(Horn.loadHorns)
-}
+    .then(Horn.populateFilter)
+    .then(Horn.handleFilter);
+};
+
+// creates function that iterates through the horns array and renders each object to the page.
 
 Horn.loadHorns = () => {
   Horn.allHorns.forEach(horn => horn.render())
 }
 
+Horn.populateFilter = () => {
+  let filterKeywords = [];
+  $('option').not(':first').remove();
+  Horn.all.forEach(horn => {
+    if (!filterKeywords.includes(horn.keyword))
+    filterKeywords.push(horn.keyword);
+  });
+
+  filterKeywrods.sort();
+
+  filterKeywords.forEach(keyword => {
+    let optionTag = `<option value="${keyword}">
+    ${keyword}</option>`;
+    $('select').append(optionTag);
+  });
+};
+
+Horn.handleFilter = () => {
+  $('select').on('change',function () {
+    let $selected = $(this).val();
+    if ($slected !== 'default') {
+      $(div).hide();
+
+      Horn.all.forEach(horn =>{
+        if ($selected === horn.keyword) {
+          $(`div[class="${$selected}"`).addClass
+          ('filtered').fadeIn();
+        }
+      });
+
+      $(`option[value="${selected}]`).fadeIn();
+    } else {
+      $('div').removeClass('filtered').fadeIn();
+      $(`option[value=${selected}]`).fadeIn();
+    }
+  });
+}
+
+//Loads the json data
+
 $(() => Horn.readJson());
 
-$('select[name="animals"]').on('change', function (){
-  let $selection = $(this).val();
-  $('data/page-1.json').hide()
-  $(`data/page-1.json[keyword="${selection}"]`).show()
-})
+// $('select[name="animals"]').on('change', function () {
+//   let $selection = $(this).val();
+//   $('data/page-1.json').hide()
+//   $(`data/page-1.json[keyword="${selection}"]`).show()
+// })
